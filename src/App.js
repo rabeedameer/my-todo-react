1f3b7503629e2859222a93346705e37a970bd521
rabee <rabee.dameer@gmail.com>
2018-09-10 12:46:01 +0200

    created header component and imported it into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-
+import Header from './components/Header';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">My to do!</h1>
-        </header>
+        <Header/>
         <p className="App-intro">
           This is my to do list .
         </p>
