af6ca435c2148b9a569dfa18c20625a731d804fb
rabee <rabee.dameer@gmail.com>
2018-09-10 14:53:35 +0200

    created Router component and loaded it into our index.js & replaced App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from './components/Router';
 import "./css/style.css";
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
