64d05965440c6439fdf10fec9d8ebf786bc73612
rabee <rabee.dameer@gmail.com>
2018-09-10 17:51:26 +0200

    removed the un-needed import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
 import Router from './components/Router';
 import "./css/style.css";
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
