eea99af5d9f32e0a99f12546a12ad5c060d5692a
rabee <rabee.dameer@gmail.com>
2018-09-10 12:53:20 +0200

    added ToDoList component and imported it into App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-
+import ToDoList from './components/ToDoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-        <p className="App-intro">
-          This is my to do list .
-        </p>
+        <ToDoList/>
       </div>
     );
   }
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ToDoList  extends Component {
+  render(){
+    return (
+      <div>To do items will be here!</div>
+    );
+  }
+}
+
+export default ToDoList;
