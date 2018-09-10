8bf56b5c5a184b6de5e1b9d64abe95886153251a
rabee <rabee.dameer@gmail.com>
2018-09-10 17:57:27 +0200

    set handleSubmit method: displays a message & onSubmit event to the form

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,9 +1,16 @@
 import React ,{Component} from 'react';
 
 class ToDoForm extends Component{
+  handleSubmit(e){
+    e.preventDefault();
+    console.log('Create new item');
+  }
+
+
+
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           type='text'
           placeholder='Add a new item...'
